21d2f298be51d75f0c68713a9be6d1b157ca7d5c
liu <1679458763@qq.com>
2022-03-31 16:35:31 +0800

    20220331 每日一题

--- src/com/lwc/round3/BackTrackingAlgrom.java
+++ src/com/lwc/round3/BackTrackingAlgrom.java
@@ -1,4 +1,51 @@
 package com.lwc.round3;
 
+import java.util.*;
+
 public class BackTrackingAlgrom {
+    public static void main(String[] args) {
+        BackTrackingAlgrom bta=new BackTrackingAlgrom();
+        System.out.println(bta.letterCombinations(""));
+    }
+    /**
+     * 题号：17
+     * 难度：中等
+     * 时间：20220328
+     * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+     *
+     * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+     */
+    public List<String> letterCombinations(String digits) {
+        Map<Character,List<Character>> map = new HashMap<>();
+        map.put('2',new ArrayList<Character>(Arrays.asList(new Character[]{'a','b','c'})));
+        map.put('3',new ArrayList<Character>(Arrays.asList(new Character[]{'d','e','f'})));
+        map.put('4',new ArrayList<Character>(Arrays.asList(new Character[]{'g','h','i'})));
+        map.put('5',new ArrayList<Character>(Arrays.asList(new Character[]{'j','k','l'})));
+        map.put('6',new ArrayList<Character>(Arrays.asList(new Character[]{'m','o','n'})));
+        map.put('7',new ArrayList<Character>(Arrays.asList(new Character[]{'p','q','r','s'})));
+        map.put('8',new ArrayList<Character>(Arrays.asList(new Character[]{'t','u','v'})));
+        map.put('9',new ArrayList<Character>(Arrays.asList(new Character[]{'w','x','y','z'})));
+        List<String> res = new ArrayList<>();
+        if(digits.length()>0){
+            backTrack(0,map,res,digits.length(),new StringBuffer(),digits);
+        }
+        return res;
+    }
+    public void backTrack(int i, Map<Character,List<Character>> map,List<String> res,int len
+    ,StringBuffer sb,String diag){
+        //如果i小于字符串长度时说明遍历还没有到底可以接着往下一层进行遍历
+        if(i<len){
+            //先获取
+            List<Character> temp = map.get(diag.charAt(i));
+            for(Character c : temp){
+                sb.append(c);
+                backTrack(i+1,map,res,len,sb,diag);
+                //回到之前的状态
+                sb.delete(sb.length()-1,sb.length());
+            }
+        }else if(i==len){
+            //i已经到达最底层了则说明可以直接拼上去
+            res.add(sb.toString());
+        }
+    }
 }
--- src/com/lwc/round3/DayFour.java
+++ src/com/lwc/round3/DayFour.java
@@ -1,10 +1,9 @@
 package com.lwc.round3;
 
+import java.util.*;
+
 public class DayFour {
-    public static void main(String[] args) {
-        DayFour four =new DayFour();
-        four.hasAlternatingBits(10);
-    }
+
     /**
      * 题号：2028
      * 难度：中等
@@ -28,29 +27,10 @@ public class DayFour {
             tempSum+=rolls[i];
         }
         int[] ans = new int[n];
-        rollBack(sum,n,tempSum,ans,0);
+
         return ans;
     }
-    public int rollBack(int sum,int m,int tempSum,int[] ans,int time){
-        if(time<m && flag){
-            //time表示的是找到数据的个数,ans表示的是所找到的结果
-            for(int i =1;i<7;i++){
-                tempSum+=i;
-                ans[time++]=i;
-                //如果tempSum大于sum的话则进行剪枝
-                if(tempSum<=sum){
-                    if(rollBack(sum,m,tempSum,ans,time) == sum){
-                        flag = false;
-                        return tempSum;
-                    }
-                }
-                //重置状态
-                ans[--time]=0;
-                tempSum-=i;
-            }
-        }
-      return tempSum;
-    }
+
     public int[] missingRolls2(int[] rolls, int mean, int n) {
         int sum = mean *(rolls.length +n);
         //减去原来有的数据
@@ -95,5 +75,217 @@ public class DayFour {
         }else{
             return false;
         }
+    }
+    /**
+     * 题号：2024
+     * 难度：中等
+     * 时间：20220329
+     * 一位老师正在出一场由 n 道判断题构成的考试，每道题的答案为 true （用 'T' 表示）或者 false （用 'F' 表示）。老师想增加学生对自己做出答案的不确定性，方法是 最大化 有 连续相同 结果的题数。（也就是连续出现 true 或者连续出现 false）。
+     *
+     * 给你一个字符串 answerKey ，其中 answerKey[i] 是第 i 个问题的正确结果。除此以外，还给你一个整数 k ，表示你能进行以下操作的最多次数：
+     *
+     * 每次操作中，将问题的正确答案改为 'T' 或者 'F' （也就是将 answerKey[i] 改为 'T' 或者 'F' ）。
+     * 请你返回在不超过 k 次操作的情况下，最大 连续 'T' 或者 'F' 的数目。
+     * 解题：该题自己本身不会写看完答案后理解了答案才知道怎么写了，本题采用了滑动窗口算法进行计算
+     * 将题目转化为找整个字符串中包含少于k个T或F的最大窗口数，然后两进行比较求其最大值便时所求答案，具体解法如下
+     */
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        return Math.max(getMaxHatch(answerKey,k,'T'),getMaxHatch(answerKey,k,'F'));
+    }
+    public int getMaxHatch(String answerKey,int k ,char c){
+        int ans = 0;
+        int len = answerKey.length();
+        for(int left =0 ,right =0,count =0;right<len;right++){
+            //计算当前窗口包含字符c的个数
+            if(answerKey.charAt(right) == c){
+                count++;
+            }
+            //如果当中count大于k的话则需要减少窗口大小，从而减少包含字符c的个数
+            while(count >k){
+                //如果left对应的字符是c时则count减少
+                if(answerKey.charAt(left) == c){
+                    count--;
+                }
+                //当count>k时一直要减少窗口的大小
+                left++;
+            }
+            //计算符合条件的窗口大小,并且保留最大窗口值
+           ans = Math.max(ans,right-left+1);
+        }
+        return ans;
+    }
+    /**
+     * 题号：1004
+     * 难度：中等
+     * 时间：20220329
+     * 给定一个二进制数组 nums 和一个整数 k，如果可以翻转最多 k 个 0 ，则返回 数组中连续 1 的最大个数 。
+     * 该题目与上述题目类型一致用于训练自己对滑动窗口的理解
+     */
+    public int longestOnes(int[] nums, int k) {
+        int ans = 0;
+        int len = nums.length;
+        for(int left =0 ,count =0 ,right =0 ;right< len ;right++){
+            if(nums[right] == 0 ){
+                count++;
+            }
+            while(count>k){
+                if(nums[left] == 0){
+                    count--;
+                }
+                left++;
+            }
+           ans = Math.max(ans,right-left+1);
+        }
+        return ans;
+    }
+    /**
+     * 题号：1606
+     * 难度：困难
+     * 时间：20220330
+     *你有 k 个服务器，编号为 0 到 k-1 ，它们可以同时处理多个请求组。每个服务器有无穷的计算能力但是 不能同时处理超过一个请求 。请求分配到服务器的规则如下：
+     *
+     * 第 i （序号从 0 开始）个请求到达。
+     * 如果所有服务器都已被占据，那么该请求被舍弃（完全不处理）。
+     * 如果第 (i % k) 个服务器空闲，那么对应服务器会处理该请求。
+     * 否则，将请求安排给下一个空闲的服务器（服务器构成一个环，必要的话可能从第 0 个服务器开始继续找下一个空闲的服务器）。比方说，如果第 i 个服务器在忙，那么会查看第 (i+1) 个服务器，第 (i+2) 个服务器等等。
+     * 给你一个 严格递增 的正整数数组 arrival ，表示第 i 个任务的到达时间，和另一个数组 load ，其中 load[i] 表示第 i 个请求的工作量（也就是服务器完成它所需要的时间）。你的任务是找到 最繁忙的服务器 。最繁忙定义为一个服务器处理的请求数是所有服务器里最多的。
+     *
+     * 请你返回包含所有 最繁忙服务器 序号的列表，你可以以任意顺序返回这个列表。
+     * 分析：该题目可以采用优先队列进行解题，每次都使用最早结束的时间来进行处理最近的一个任务
+     * 即可
+     */
+    public List<Integer> busiestServers(int k, int[] arrival, int[] load) {
+        List<Integer> ans = new ArrayList<>();
+        int len = arrival.length;
+        //保存整个数组中的所有的服务数量
+        int[] counts = new int[k];
+        //定义一个优先队列其中元素是一个二维数组a[0]表示的是编号a[1]表示的是任务结束时间
+        Queue<int[]> queue = new PriorityQueue<int[]>((a,b)->{return a[1]-b[1];});
+        //定义一个treeset排序集合，用于存放空闲服务器，并且找到最接近i%k的服务器
+        TreeSet<Integer> free = new TreeSet<>();
+        //初始时所有的服务器均是空闲的
+        for(int i =0 ;i<k;i++){
+            free.add(i);
+        }
+        //用于比较最大的任务数量
+        int maxCount = 0;
+        for(int i =0;i<len ;i++){
+            //判断优先队列中时间最小的元素值，如果没有超过任务的到达时间则加到空闲集合中去
+            while(!queue.isEmpty() && queue.peek()[1]<=arrival[i]){
+                free.add(queue.poll()[0]);
+            }
+            //如果没有空闲的服务器则放弃该服务
+            if(free.size()==0){
+                continue;
+            }
+            //找到可以进行服务的服务器
+           Integer server =  free.ceiling(i%k);
+            //如果找不到i%k的服务器则需要重新从0开始进行查找可以服务的服务器
+            if(server == null){
+                server = free.ceiling(0);
+            }
+            //对应服务器服务的数量增加
+            counts[server]++;
+            if(counts[server]>maxCount){
+                ans.clear();
+                ans.add(server);
+                maxCount=counts[server];
+            }else if(counts[server] == maxCount){
+                ans.add(server);
+            }
+            //将其从空闲集合中移除
+            free.remove(server);
+            //加入到优先队列中
+            queue.add(new int[]{server,arrival[i]+load[i]});
+
+        }
+        return ans;
+    }
+    /**
+     * 题号：728
+     * 难度：简单
+     * 时间：20220331
+     * 自除数 是指可以被它包含的每一位数整除的数。
+     *
+     * 例如，128 是一个 自除数 ，因为 128 % 1 == 0，128 % 2 == 0，128 % 8 == 0。
+     * 自除数 不允许包含 0 。
+     *
+     * 给定两个整数 left 和 right ，返回一个列表，列表的元素是范围 [left, right] 内所有的 自除数
+     */
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i = left ;i<=right;i++){
+            String str = i+"";
+            char[] chars = str.toCharArray();
+            int count = 0;
+            for(char c :chars){
+                int j=Integer.parseInt(c+"");
+                if(j != 0){
+                    if(i%j == 0){
+                        count++;
+                        if(count==chars.length){
+                            ans.add(i);
+                        }
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 求出大于或等于 N 的最小回文素数。
+     *
+     * 回顾一下，如果一个数大于 1，且其因数只有 1 和它自身，那么这个数是素数。
+     *
+     * 例如，2，3，5，7，11 以及 13 是素数。
+     *
+     * 回顾一下，如果一个数从左往右读与从右往左读是一样的，那么这个数是回文数。
+     *
+     * 例如，12321 是回文数。
+     */
+    public int primePalindrome(int n) {
+        double MAX_VAL = 2*1e9;
+        int temp = n+1;
+        while(temp<MAX_VAL){
+            if(isBack(temp) && isPrime(temp)){
+                return temp;
+            }else{
+                temp++;
+            }
+        }
+        return 0;
+    }
+    public boolean isPrime(int num){
+        if(num == 1){
+            return false;
+        }
+        int tmep =(int) Math.sqrt(num);
+        for(int i =2;i<=tmep ;i++){
+            if(num%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean isBack(int num){
+        String str = num+"";
+        char[] chars =str.toCharArray();
+        int j=0,i=chars.length-1;
+        while(i>j){
+            if(chars[i] == chars[j]){
+                i--;
+                j++;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        DayFour four =new DayFour();
+        System.out.println(four.primePalindrome(9989900));
     }
 }
